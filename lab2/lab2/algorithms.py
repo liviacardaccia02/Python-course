c60694f4bda825b3670a7cbbac265e3c4f41daec
liviacardaccia02 <liviacardaccia99@gmail>
2025-11-19 16:17:34 +0100

    feat: enhance dfs function with cycle detection and step printing options, added bfs implementation

--- lab2/lab2/algorithms.py
+++ lab2/lab2/algorithms.py
@@ -1,10 +1,38 @@
 from basicgraph import adjacent
 
-def dfs(graph, start, visited, parent):
+def dfs(graph, start, visited=None, parent=None, print_steps=True):
+    if visited is None:
+        visited = set()
+    
     visited.add(start)
+    
+    is_acyclic = True 
+    
     for neighbor in adjacent(graph, start):
         if neighbor not in visited:
-            dfs(graph, neighbor, visited, parent=start)
+            print(f"go to {neighbor}") if print_steps else None
+
+            child_is_acyclic, _ = dfs(graph, neighbor, visited, parent=start, print_steps=print_steps)
+            
+            if not child_is_acyclic:
+                is_acyclic = False
+
+            print(f"go to {start}") if print_steps else None
+            
         elif parent is not None and neighbor != parent:
-            return False # cycle detected
-    return True
+            is_acyclic = False
+
+    return is_acyclic, visited
+
+def bfs(graph, node):
+    pred = {node: None}
+    dist = {node: 0}
+    queue = [node]  
+    while queue:
+        current = queue.pop(0)
+        for neighbor in adjacent(graph, current):
+            if neighbor not in dist:
+                pred[neighbor] = current
+                dist[neighbor] = dist[current] + 1
+                queue.append(neighbor)
+    return pred, dist
