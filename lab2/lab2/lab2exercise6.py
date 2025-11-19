b9b785d72e50f00012742ec8d76d04657199cc86
liviacardaccia02 <liviacardaccia99@gmail>
2025-11-19 16:21:21 +0100

    feat: add radius calculation for graphs with user input

---
+++ lab2/lab2/lab2exercise6.py
@@ -0,0 +1,23 @@
+from basicgraph import read_graph, random_node
+from lab2exercise5 import bfs
+
+def radius(graph): 
+    if not graph:
+        return 0
+    
+    start_node = next(iter(graph))
+    _, dist_from_start = bfs(graph, start_node)
+    
+    node_a = max(dist_from_start, key=dist_from_start.get)
+    _, dist_from_a = bfs(graph, node_a)
+    max_distance = max(dist_from_a.values())
+    
+    return max_distance
+
+def main():
+    filepath = input("Enter the file path: ")
+    graph = read_graph(filepath)
+    print("radius is", radius(graph))
+
+if __name__ == "__main__":
+    main()
